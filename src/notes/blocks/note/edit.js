5520bfa9d078affd2e9e8e1c4916b27482f1c506
Artur Piszek <artur.piszek@gmail.com>
2024-04-10 11:14:54 +0200

    Notes block with embeddable notes (#8)
    * Somehow this is working
    
    * everything works with embedding content
    
    * save serialized content for posterity
    
    * style
    
    * link to original note
    
    * group block
    
    * transforms

---
+++ src/notes/blocks/note/edit.js
@@ -0,0 +1,87 @@
+/**
+ * WordPress dependencies
+ */
+
+import { useBlockProps, useInnerBlocksProps, InspectorControls } from '@wordpress/block-editor';
+import { useEffect } from "@wordpress/element";
+import { useDispatch, useSelect } from '@wordpress/data';
+import { serialize, parse } from '@wordpress/blocks';
+import { PanelBody } from '@wordpress/components';
+
+import './index.css';
+
+const Edit = ( props ) => {
+	const {
+		attributes: { note_id },
+		setAttributes,
+	} = props;
+
+	const blockProps = useBlockProps();
+	const { children, ...innerBlocksProps } = useInnerBlocksProps();
+
+	const { saveEntityRecord, editEntityRecord } = useDispatch( 'core' );
+	const { replaceInnerBlocks } = useDispatch( 'core/block-editor' );
+
+	const localEmbeddedContent = useSelect( ( select ) => {
+		const { getBlocksByClientId } = select( 'core/block-editor' );
+		const childrenBlocks = getBlocksByClientId( children?.props?.clientId )[0]?.innerBlocks || [];
+		return childrenBlocks ? serialize( childrenBlocks ) : "";
+	}, [ children ] );
+
+	const remoteEmbedded = useSelect( ( select ) => {
+		if ( ! note_id ) {
+			return null;
+		}
+		return select('core').getEntityRecord( 'postType', 'notes', note_id );
+	}, [ note_id ] );
+
+	const remoteEmbeddedContent = remoteEmbedded?.content?.raw;
+	useEffect( () => {
+		if ( ! note_id ) {
+			saveEntityRecord( 'postType', 'notes', { title: 'Embedded Note', status: 'draft', content: localEmbeddedContent } ).then( note => {
+				setAttributes( { note_id: note.id } );
+			} );
+		} else if ( remoteEmbedded && remoteEmbedded.content ) {
+			replaceInnerBlocks( props?.clientId, parse( remoteEmbeddedContent ) );
+		}
+	}, [note_id, remoteEmbeddedContent ] );
+
+	useEffect( () => {
+		if( ! note_id ) {
+			return;
+		}
+		if ( remoteEmbeddedContent === localEmbeddedContent ) {
+			return;
+		}
+		if ( ! localEmbeddedContent || localEmbeddedContent.length < 5 ) {
+			return;
+		}
+		if( ! remoteEmbeddedContent ) {
+			return;
+		}
+		console.log( 'saving new content', localEmbeddedContent, remoteEmbeddedContent );
+		editEntityRecord( 'postType', 'notes', note_id, { id: note_id, content: localEmbeddedContent } );
+	}, [ localEmbeddedContent, remoteEmbeddedContent, note_id ] );
+
+
+	return (
+		<div { ...blockProps }>
+			{ note_id && ( <InspectorControls>
+				<PanelBody title={ 'Note' }>
+					<p>
+						<a
+							target="_blank"
+							href={ `/wp-admin/post.php?post=${ note_id }&action=edit` }
+						>
+							Open original note
+						</a>
+					</p>
+				</PanelBody>
+			</InspectorControls> ) }
+			<div { ...innerBlocksProps }>
+				{ children }
+			</div>
+		</div>
+	);
+};
+export default Edit;
---
+++ src/notes/blocks/note/index.js
@@ -0,0 +1,68 @@
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockType, createBlock } from '@wordpress/blocks';
+
+
+/**
+ * Internal dependencies
+ */
+import Edit from './edit';
+import save from './save';
+import metadata from './block.json';
+
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+const selfURL = new URL( document.baseURI );
+const regex = /post\.php\?post=([0-9]+)\&action=edit/i;
+registerBlockType( metadata.name, {
+	/**
+	 * @see ./edit.js
+	 */
+	edit: Edit,
+
+	/**
+	 * @see ./save.js
+	 */
+	save,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/group' ],
+				transform: ( attributes, innerBlocks ) => {
+					return createBlock( metadata.name, {
+						note_id: 0,
+					}, innerBlocks );
+				},
+			},
+			{
+				type: 'raw',
+				isMatch: ( node ) => {
+					if ( node.nodeName !== 'P' ) {
+						return false;
+					}
+					const content = node.textContent;
+					if ( ! content.includes( selfURL.origin ) ) {
+						return false;
+					}
+					// Now we are talking. This may be it.
+					return regex.test( content );
+				},
+				transform: ( node ) => {
+					// TODO: Disallow embedding other post types
+					const match = node.textContent.match( regex );
+					const note_id = parseInt( match[1] );
+					return createBlock( metadata.name, {
+						note_id,
+					} );
+				},
+			},
+		]
+	}
+} );
