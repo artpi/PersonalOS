096e2b15ce7983aff79553cbd0e65149b08d2157
artpi <artur.piszek@gmail.com>
2024-06-11 11:35:32 +0200

    unit tests

---
+++ tests/SampleTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Personalos
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
