f8f8716c2a2f0d60c217b663f92e68f84c7f6937
artpi <artur.piszek@gmail.com>
2025-01-04 16:01:08 +0100

    Workflow to create a release and a version bump

--- personalos.php
+++ personalos.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Personal OS
  * Description:     Manage your life.
- * Version:         0.0.2
+ * Version:         0.2.1
  * Author:          Artur Piszek (artpi)
  * Author URI:      https://piszek.com
  * License:         GPL-2.0-or-later
@@ -16,7 +16,7 @@
 
 class POS {
 	public static $modules = array();
-	public static $version = '0.0.1';
+	public static $version = '0.2.1';
 
 	public static function init() {
 		add_action( 'admin_menu', array( 'POS', 'admin_menu' ) );
